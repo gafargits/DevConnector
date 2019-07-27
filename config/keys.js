2ff3f3c896b7b9559292b8ec60253b1f5c913cac
gafaronline <gafaronline@hotmail.com>
2019-07-28 00:47:45 +0100

    Initial setup of the express server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://Gafar:@Boo123ect@devconnector-ihtiv.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');;
+
+const app = express();
+
+//DB Config
+const db =  require('./config/keys').mongoURI;
+
+//connect to mongoDB
+mongoose
+    .connect(db)
+    .then(() => {
+        return console.log('mongoDB connected')
+    })
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    return res.send('hello world again!');
+});
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    return console.log(`Server running on port ${port}`)
+})
