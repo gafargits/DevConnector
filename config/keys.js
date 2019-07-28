27ee154b3ad6ae23e0f2e6001bd6da4ee8880bea
gafaronline <gafaronline@hotmail.com>
2019-07-28 15:28:48 +0100

    jwt implemented

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://Gafar:@Boo123ect@devconnector-ihtiv.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI : 'mongodb+srv://Gafar:@Boo123ect@devconnector-ihtiv.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey : 'mysecret'
 }
